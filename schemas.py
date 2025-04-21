73848a9a8d114719ac37dcc4abfd3ce6e0e129b2
Jomprii <jyamson30@gmail.com>
2025-04-22 02:05:21 +0800

    New

--- schemas.py
+++ schemas.py
@@ -6,8 +6,8 @@ class TaskBase(BaseModel):
 
 class TaskCreate(BaseModel):
     title: str
-    description: str
-    
+    completed: bool = False
+
 class TaskUpdate(BaseModel):
     title: str | None = None
     completed: bool | None = None
